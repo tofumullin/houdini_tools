1c9143c736cd47f72d0415d1715ee65a37a442a8
tofumullin <tofu_wood@126.com>
2018-06-20 14:59:48 +0800

    debug something
    modify the slidebar , combobox

--- wedge_filter_tool.py
+++ wedge_filter_tool.py
@@ -130,25 +130,21 @@ class Ui_MainWindow(object):
         # initial widgets
         self.btn_language.toggle()
         self.btn_language.setCheckable(True)
-        self.slider_view.setMaximum(400)
+        self.slider_view.setMaximum(800)
         self.slider_view.setMinimum(20)
         
         self.tableWidget.initUI('')
         
-        # self.comboBox_sort.addItems(self.tableWidget.getHeader())
+        self.comboBox_sort.addItems(['$HIPNAME.$OS.$WEDGE.$F.exr','$HIPNAME.$OS.$WEDGE.exr'])
         self.edit_node.setText('/out/wedge1')
         self.statusbar.showMessage('Ready')
-
-        # fileMenu = self.menubar.addMenu('File')
-        # openMenu = self.menubar.addMenu('Open')
-
 
         # connect the functions
         
         self.btn_listView.clicked.connect(self.clickListView)
         self.btn_gridView.clicked.connect(self.clickGridView)
         
-        self.comboBox_sort.currentIndexChanged.connect(self.sortchange)
+        self.comboBox_sort.currentIndexChanged.connect(self.templateChange)
 
         self.btn_node.clicked.connect(lambda:self.clickAddPicture(self.btn_addPic))
         self.btn_exNode.clicked.connect(self.msgwarning)
@@ -168,7 +164,6 @@ class Ui_MainWindow(object):
         self.btn_back.setEnabled(0)
         self.btn_listView.setEnabled(0)
         self.btn_gridView.setEnabled(0)
-        self.comboBox_sort.setEnabled(0)
         self.slider_view.setEnabled(0)
         self.btn_appPic.setEnabled(0)
         self.btn_next.setEnabled(0)
@@ -182,7 +177,7 @@ class Ui_MainWindow(object):
         self.btn_gridView.setText(QApplication.translate("MainWindow", "Grid", None, -1))
         self.btn_listView.setText(QApplication.translate("MainWindow", "List", None, -1))
         self.btn_language.setText(QApplication.translate("MainWindow", "EN", None, -1))
-        self.sortlabel.setText(QApplication.translate("MainWindow", "sort by", None, -1))
+        self.sortlabel.setText(QApplication.translate("MainWindow", "filename template", None, -1))
         self.btn_addPic.setText(QApplication.translate("MainWindow", "Add Picture", None, -1))
         self.btn_appPic.setText(QApplication.translate("MainWindow", "Append Picture", None, -1))
         self.btn_back.setText(QApplication.translate("MainWindow", "Back", None, -1))
@@ -200,7 +195,7 @@ class Ui_MainWindow(object):
         self.btn_gridView.setText(QApplication.translate("MainWindow", "网格显示", None, -1))
         self.btn_listView.setText(QApplication.translate("MainWindow", "列表显示", None, -1))
         self.btn_language.setText(QApplication.translate("MainWindow", "CN", None, -1))
-        self.sortlabel.setText(QApplication.translate("MainWindow", "排序依据", None, -1))
+        self.sortlabel.setText(QApplication.translate("MainWindow", "文件名格式", None, -1))
         self.btn_addPic.setText(QApplication.translate("MainWindow", "添加图片", None, -1))
         self.btn_appPic.setText(QApplication.translate("MainWindow", "追加图片", None, -1))
         self.btn_back.setText(QApplication.translate("MainWindow", "返回上一步", None, -1))
@@ -226,9 +221,9 @@ class Ui_MainWindow(object):
 
     def sliderval(self):
         self.tableWidget.cellresize(self.slider_view.value())
-        if self.slider_view.value()>100:
+        if self.tableWidget.isGridView:
             self.tableWidget.gridview()
-        else:
+        else:   
             self.tableWidget.listview()
         
     def clickGridView(self):
@@ -243,45 +238,43 @@ class Ui_MainWindow(object):
         self.tableWidget.listview()
         
 
-    def sortchange(self,i):
-        self.tableWidget.sortItems(i)
-        for count in range(self.comboBox_sort.count()):
-            print( 'item'+str(count) + '='+ self.comboBox_sort.itemText(count) )
-            print( "Current index",i,"selection changed ",self.comboBox_sort.currentText() )
-
-    def backfilter(self):
-        files = self.oldfiles
-        # print files
+    def templateChange(self):
+        files = self.tableWidget.nextFilterItem()[0]
         if files != []:
             self.tableWidget.clear()
-            self.tableWidget.initUI(files)
+            self.tableWidget.initUI(files,self.comboBox_sort.currentIndex())
+            if self.tableWidget.isGridView : self.tableWidget.gridview()
+            else : self.tableWidget.listview()
+
+        
+    def backfilter(self):
+        files = self.oldfiles
+        if files != []:
+            self.tableWidget.clear()
+            self.tableWidget.initUI(files,self.comboBox_sort.currentIndex())
             if self.tableWidget.isGridView : self.tableWidget.gridview()
             else : self.tableWidget.listview()
 
     def nextfilter(self):
         self.oldfiles = self.tableWidget.nextFilterItem()[0]
         files = self.tableWidget.nextFilterItem()[1]
-        self.tableWidget.isGridView = self.tableWidget.nextFilterItem()[2]
-        # print files
-        # print self.tableWidget.nextFilterItem()[3]
-        if files != []:
 
+        if files != []:
             self.tableWidget.clear()
-            self.tableWidget.initUI(files)
+            self.tableWidget.initUI(files,self.comboBox_sort.currentIndex())
             if self.tableWidget.isGridView : self.tableWidget.gridview()
             else : self.tableWidget.listview()
 
     def getfiles(self):
-        files,_ = QFileDialog.getOpenFileNames(self.btn_addPic ,
+        files, ok = QFileDialog.getOpenFileNames(self.btn_addPic ,
                                     "Select one or more files to open",
                                      "/home",
-                                     "Images (*.png *.xpm *.jpg *.jpeg *.exr)")
+                                     "Images (*.png *.xpm *.jpg *.jpeg)")
         # files = [u'/Users/mullin/Downloads/wedge_filter/render/wedge_test.mantra1._wedge_translatey_-0.009130_translatex_-0.101018_scaley_0.825796_scalex_0.894362.0001.jpg', u'/Users/mullin/Downloads/wedge_filter/render/wedge_test.mantra1._wedge_translatey_-0.124225_translatex_-0.008376_scaley_0.616542_scalex_0.615433.0001.jpg', u'/Users/mullin/Downloads/wedge_filter/render/wedge_test.mantra1._wedge_translatey_-0.210204_translatex_0.151692_scaley_0.660623_scalex_0.815474.0001.jpg', u'/Users/mullin/Downloads/wedge_filter/render/wedge_test.mantra1._wedge_translatey_-0.212801_translatex_-0.659302_scaley_0.751119_scalex_0.991038.0001.jpg', u'/Users/mullin/Downloads/wedge_filter/render/wedge_test.mantra1._wedge_translatey_-0.250594_translatex_-0.122077_scaley_0.754213_scalex_0.889221.0001.jpg', u'/Users/mullin/Downloads/wedge_filter/render/wedge_test.mantra1._wedge_translatey_-0.387227_translatex_0.717029_scaley_0.655182_scalex_0.969644.0001.jpg', u'/Users/mullin/Downloads/wedge_filter/render/wedge_test.mantra1._wedge_translatey_-0.461441_translatex_0.095993_scaley_0.978558_scalex_0.502855.0001.jpg', u'/Users/mullin/Downloads/wedge_filter/render/wedge_test.mantra1._wedge_translatey_-0.489412_translatex_0.683490_scaley_0.836557_scalex_0.541617.0001.jpg', u'/Users/mullin/Downloads/wedge_filter/render/wedge_test.mantra1._wedge_translatey_-0.542476_translatex_0.890541_scaley_0.950714_scalex_0.515295.0001.jpg', u'/Users/mullin/Downloads/wedge_filter/render/wedge_test.mantra1._wedge_translatey_-0.562438_translatex_-0.080793_scaley_0.644891_scalex_0.510745.0001.jpg', u'/Users/mullin/Downloads/wedge_filter/render/wedge_test.mantra1._wedge_translatey_-0.566801_translatex_-0.155767_scaley_0.514520_scalex_0.610846.0001.jpg', u'/Users/mullin/Downloads/wedge_filter/render/wedge_test.mantra1._wedge_translatey_-0.588476_translatex_0.348306_scaley_0.716475_scalex_0.597059.0001.jpg', u'/Users/mullin/Downloads/wedge_filter/render/wedge_test.mantra1._wedge_translatey_-0.653985_translatex_0.097598_scaley_0.851520_scalex_0.837243.0001.jpg', u'/Users/mullin/Downloads/wedge_filter/render/wedge_test.mantra1._wedge_translatey_-0.680749_translatex_0.054761_scaley_0.584072_scalex_0.636457.0001.jpg', u'/Users/mullin/Downloads/wedge_filter/render/wedge_test.mantra1._wedge_translatey_-0.707077_translatex_0.437671_scaley_0.580114_scalex_0.852303.0001.jpg', u'/Users/mullin/Downloads/wedge_filter/render/wedge_test.mantra1._wedge_translatey_-0.731272_translatex_0.694867_scaley_0.881887_scalex_0.627535.0001.jpg', u'/Users/mullin/Downloads/wedge_filter/render/wedge_test.mantra1._wedge_translatey_-0.781023_translatex_0.249604_scaley_0.672211_scalex_0.534758.0001.jpg', u'/Users/mullin/Downloads/wedge_filter/render/wedge_test.mantra1._wedge_translatey_-0.782477_translatex_0.799637_scaley_0.755058_scalex_0.604545.0001.jpg', u'/Users/mullin/Downloads/wedge_filter/render/wedge_test.mantra1._wedge_translatey_-0.791152_translatex_0.331915_scaley_0.648036_scalex_0.749900.0001.jpg', u'/Users/mullin/Downloads/wedge_filter/render/wedge_test.mantra1._wedge_translatey_-0.812281_translatex_-0.943305_scaley_0.917883_scalex_0.716384.0001.jpg', u'/Users/mullin/Downloads/wedge_filter/render/wedge_test.mantra1._wedge_translatey_-0.882430_translatex_-0.402788_scaley_0.983952_scalex_0.937767.0001.jpg', u'/Users/mullin/Downloads/wedge_filter/render/wedge_test.mantra1._wedge_translatey_-0.887753_translatex_0.740020_scaley_0.785000_scalex_0.599920.0001.jpg', u'/Users/mullin/Downloads/wedge_filter/render/wedge_test.mantra1._wedge_translatey_-0.913025_translatex_0.406764_scaley_0.991594_scalex_0.796592.0001.jpg', u'/Users/mullin/Downloads/wedge_filter/render/wedge_test.mantra1._wedge_translatey_-0.930948_translatex_-0.514520_scaley_0.898702_scalex_0.707157.0001.jpg', u'/Users/mullin/Downloads/wedge_filter/render/wedge_test.mantra1._wedge_translatey_-0.944050_translatex_-0.540790_scaley_0.588606_scalex_0.792230.0001.jpg', u'/Users/mullin/Downloads/wedge_filter/render/wedge_test.mantra1._wedge_translatey_-0.949108_translatex_0.082825_scaley_0.969575_scalex_0.690602.0001.jpg', u'/Users/mullin/Downloads/wedge_filter/render/wedge_test.mantra1._wedge_translatey_-0.952731_translatex_-0.226886_scaley_0.710459_scalex_0.594020.0001.jpg', u'/Users/mullin/Downloads/wedge_filter/render/wedge_test.mantra1._wedge_translatey_-0.966619_translatex_-0.970880_scaley_0.877793_scalex_0.624780.0001.jpg', u'/Users/mullin/Downloads/wedge_filter/render/wedge_test.mantra1._wedge_translatey_0.009441_translatex_-0.030150_scaley_0.678395_scalex_0.673039.0001.jpg', u'/Users/mullin/Downloads/wedge_filter/render/wedge_test.mantra1._wedge_translatey_0.027543_translatex_0.904935_scaley_0.788897_scalex_0.729566.0001.jpg', u'/Users/mullin/Downloads/wedge_filter/render/wedge_test.mantra1._wedge_translatey_0.041877_translatex_-0.213490_scaley_0.744847_scalex_0.514787.0001.jpg', u'/Users/mullin/Downloads/wedge_filter/render/wedge_test.mantra1._wedge_translatey_0.076958_translatex_0.246979_scaley_0.806226_scalex_0.729073.0001.jpg', u'/Users/mullin/Downloads/wedge_filter/render/wedge_test.mantra1._wedge_translatey_0.140561_translatex_-0.656966_scaley_0.933891_scalex_0.986888.0001.jpg', u'/Users/mullin/Downloads/wedge_filter/render/wedge_test.mantra1._wedge_translatey_0.211297_translatex_0.634079_scaley_0.510409_scalex_0.508932.0001.jpg', u'/Users/mullin/Downloads/wedge_filter/render/wedge_test.mantra1._wedge_translatey_0.356352_translatex_0.089404_scaley_0.610300_scalex_0.987797.0001.jpg', u'/Users/mullin/Downloads/wedge_filter/render/wedge_test.mantra1._wedge_translatey_0.408046_translatex_0.017747_scaley_0.688984_scalex_0.673465.0001.jpg', u'/Users/mullin/Downloads/wedge_filter/render/wedge_test.mantra1._wedge_translatey_0.423180_translatex_-0.090597_scaley_0.661001_scalex_0.736886.0001.jpg', u'/Users/mullin/Downloads/wedge_filter/render/wedge_test.mantra1._wedge_translatey_0.442969_translatex_0.422384_scaley_0.968220_scalex_0.711053.0001.jpg', u'/Users/mullin/Downloads/wedge_filter/render/wedge_test.mantra1._wedge_translatey_0.487684_translatex_-0.167655_scaley_0.626179_scalex_0.504240.0001.jpg', u'/Users/mullin/Downloads/wedge_filter/render/wedge_test.mantra1._wedge_translatey_0.524560_translatex_-0.995788_scaley_0.722694_scalex_0.860770.0001.jpg', u'/Users/mullin/Downloads/wedge_filter/render/wedge_test.mantra1._wedge_translatey_0.541046_translatex_0.079235_scaley_0.930145_scalex_0.616088.0001.jpg', u'/Users/mullin/Downloads/wedge_filter/render/wedge_test.mantra1._wedge_translatey_0.567310_translatex_0.640972_scaley_0.943090_scalex_0.870252.0001.jpg', u'/Users/mullin/Downloads/wedge_filter/render/wedge_test.mantra1._wedge_translatey_0.595622_translatex_0.033199_scaley_0.611598_scalex_0.824253.0001.jpg', u'/Users/mullin/Downloads/wedge_filter/render/wedge_test.mantra1._wedge_translatey_0.618280_translatex_0.037357_scaley_0.780679_scalex_0.713045.0001.jpg', u'/Users/mullin/Downloads/wedge_filter/render/wedge_test.mantra1._wedge_translatey_0.660071_translatex_0.340611_scaley_0.651684_scalex_0.793790.0001.jpg', u'/Users/mullin/Downloads/wedge_filter/render/wedge_test.mantra1._wedge_translatey_0.675156_translatex_0.112909_scaley_0.821147_scalex_0.592953.0001.jpg', u'/Users/mullin/Downloads/wedge_filter/render/wedge_test.mantra1._wedge_translatey_0.722018_translatex_0.596878_scaley_0.898549_scalex_0.908219.0001.jpg', u'/Users/mullin/Downloads/wedge_filter/render/wedge_test.mantra1._wedge_translatey_0.757436_translatex_-0.924167_scaley_0.909707_scalex_0.981101.0001.jpg', u'/Users/mullin/Downloads/wedge_filter/render/wedge_test.mantra1._wedge_translatey_0.764958_translatex_0.692395_scaley_0.752642_scalex_0.794501.0001.jpg', u'/Users/mullin/Downloads/wedge_filter/render/wedge_test.mantra1._wedge_translatey_0.985087_translatex_0.719893_scaley_0.560445_scalex_0.666348.0001.jpg']
-        # print files
 
-        if files != []:
+        if ok:
             self.tableWidget.clear()
-            self.tableWidget.initUI(files)
+            self.tableWidget.initUI(files,self.comboBox_sort.currentIndex())
             self.tableWidget.listview()
             self.btn_back.setEnabled(1)
             self.btn_listView.setEnabled(1)
@@ -295,14 +288,14 @@ class Ui_MainWindow(object):
     def appendfiles(self):
         self.oldfiles = self.tableWidget.nextFilterItem()[0]
         self.tableWidget.isGridView = self.tableWidget.nextFilterItem()[2]
-        files,_ = QFileDialog.getOpenFileNames(self.btn_appPic ,
+        files, ok = QFileDialog.getOpenFileNames(self.btn_appPic ,
                                     "Select one or more files to open",
                                      "/home",
-                                     "Images (*.png *.xpm *.jpg *.jpeg *.exr)")
+                                     "Images (*.png *.xpm *.jpg *.jpeg)")
         # dic = QFileDialog()
         # dic.setFileMode(QFileDialog.Directory)
         # files,_ = dic.getOpenFileNames()
-        if files != []:
+        if ok:
             newfiles = self.oldfiles
             for file in files:
                 if file not in self.oldfiles:
@@ -323,27 +316,36 @@ class Ui_MainWindow(object):
         ret = msgBox.exec_()
 
     def savetxt(self):
-        txtfile, _ = QFileDialog.getSaveFileName()
-        text_file = open(txtfile, "w")
-        text_file.write(self.tableWidget.printList())
-        text_file.close()
-        # print (txtfile)
+        txtfile, ok = QFileDialog.getSaveFileName()
+        if ok:
+            text_file = open(txtfile, "w")
+            text_file.write(self.tableWidget.printList())
+            text_file.close()
+        
 
     def convertHelp(self):
-        msgBox = QMessageBox()
+        msgBox = QMessageBox() 
         msgBox.setIcon(QMessageBox.Question)
         # QMessageBox.Warning,,QMessageBox.Critical,,QMessageBox.Question
         msgBox.setText("iconvert in commandLine (exr -> jpg)")
-        msgBox.setInformativeText("Mac OS: \nfor f in *.exr; do iconvert $f $f.jpg; done \nfor f in *.jpg; do mv $f ${f/.exr.jpg/.jpg}; done ")
+        msgBox.setInformativeText(
+'''
+Mac OS:
+for f in *.exr; do iconvert $f $f.jpg; done 
+for f in *.jpg; do mv $f ${f/.exr.jpg/.jpg}; done 
+
+Windows: 
+rename *.exr *. 
+for /r %i in (*) do iconvert %i %i.jpg&&iconvert %i %i.exr 
+'''
+        )
         msgBox.setStandardButtons(QMessageBox.Ok )#| QMessageBox.Discard | QMessageBox.Cancel)
         msgBox.setDefaultButton(QMessageBox.Ok)
         ret = msgBox.exec_()
 
 #TABLE VIEWER DEFINE
 class TableWidget(QTableWidget):
-    def initUI(self, files):
-        # self.cellresize(30)
-        # self.isGridView = 0
+    def initUI(self, files,template=0):
         self.setgrid(1, 1)
         self.setShowGrid(False)
         self.setEditTriggers(0)
@@ -355,7 +357,7 @@ class TableWidget(QTableWidget):
         self.setContextMenuPolicy(Qt.CustomContextMenu)######允许右键产生子菜单
         self.customContextMenuRequested.connect(self.generateMenu)   ####右键菜单
         
-        self.pictures=FileNameToWedge(files)
+        self.pictures=FileNameToWedge(files,template)
 
     def getHeader(self):
         return self.header
@@ -364,13 +366,12 @@ class TableWidget(QTableWidget):
         newfiles = []
         oldfiles = []
         indexes = []
-
         for i in range(len(self.pictures)):
             oldfiles.append(self.pictures[i])
             if self.pictures.isSelected(i):
                 newfiles.append(self.pictures[i])
                 indexes.append(i)
-        return oldfiles, newfiles, self.isGridView, indexes
+        return oldfiles, newfiles, indexes
         
     
     def getItem(self, item):
@@ -405,13 +406,12 @@ class TableWidget(QTableWidget):
         self.setColumnCount(columenum)
         self.setRowCount(rownum)
 
-
     def printList(self):
         return self.pictures.printList()
 
     def gridview(self):
         self.isGridView = 1
-        self.selectIndexes = self.nextFilterItem()[3]
+        self.selectIndexes = self.nextFilterItem()[2]
         self.clear()
         self.setColumnCount(self.width() / self.rowHeight(0))
         self.setRowCount(len(self.pictures)/self.columnCount()+1)
@@ -436,7 +436,7 @@ class TableWidget(QTableWidget):
 
     def listview(self):
         self.isGridView = 0
-        self.selectIndexes = self.nextFilterItem()[3]
+        self.selectIndexes = self.nextFilterItem()[2]
         self.columenum = self.pictures.wedgeAmount()+1
         self.rownum    = len(self.pictures)
         self.setgrid(self.columenum, self.rownum)
@@ -511,16 +511,24 @@ class TableWidget(QTableWidget):
             return
 
 class FileNameToWedge():
-    def __init__(self,filelist):
+    def __init__(self,filelist,template=0):
+        # template: 0 = $HIPNAME.$OS.$WEDGE.$F.exr
+        # template: 1 = $HIPNAME.$OS.$WEDGE.exr
         self.wedges = []
+        self.template = template
         self.len  = len(filelist)
         self.image = filelist
         self.selection = []
         for i in range(self.len):
             self.selection.append(False)
             self.filename = filelist[i].split('/')[-1].split('_wedge_')[-1]
-            self.wedges.append(('.'.join(self.filename.split('.')[:-2])).split('_'))
-    
+            if self.template == 0:
+                self.wedges.append(('.'.join(self.filename.split('.')[:-2])).split('_'))
+            elif self.template == 1:
+                self.wedges.append(('.'.join(self.filename.split('.')[:-1])).split('_'))
+
+
+
     def __getitem__(self, key):
         return self.image[key]
 
